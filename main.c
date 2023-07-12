beb5c4cfa62c5e11358fd1059ae06b58e2e41abf
Racoonchyk <72915167+Racoonchyk@users.noreply.github.com>
2023-07-12 23:14:55 +0300

    Increase in types of goods

--- main.c
+++ main.c
@@ -7,7 +7,8 @@ typedef struct {
     int id;
     char name[100];
     char brand[20];
-    float price, memory;
+    char compatibility[100];
+    float price, memory, leigth, speed;
 } Product;
 
 typedef struct {
@@ -43,12 +44,34 @@ void displayCart(CartItem* cart, int cartSize) {
     }
 }
 
-void listProducts(Product* inventory, int count) {
+void listOfPhones(Product* inventoryOfPhones, int count) {
     printf("Список продуктів:\n");
     for (int i = 0; i < count; i++) {
-        printf("ID: %d, Назва: %s %s, Пам'ять: %.0f GB, Ціна: %.2f грн\n", inventory[i].id, inventory[i].brand, inventory[i].name, inventory[i].memory, inventory[i].price);
+        printf("ID: %d, Назва: %s %s, Пам'ять: %.0f GB, Ціна: %.2f грн\n", inventoryOfPhones[i].id, inventoryOfPhones[i].brand, inventoryOfPhones[i].name, inventoryOfPhones[i].memory, inventoryOfPhones[i].price);
     }
 }
+
+void listOfChargers(Product* inventoryOfChargers, int countChargers) {
+    printf("Список продуктів:\n");
+    for (int i = 0; i < countChargers; i++) {
+        printf("ID: %d, Назва: %s %s, Довжина: %.0f GB, Ціна: %.2f грн\n", inventoryOfChargers[i].id, inventoryOfChargers[i].brand, inventoryOfChargers[i].name, inventoryOfChargers[i].leigth, inventoryOfChargers[i].price);
+    }
+}
+
+void listOfCase(Product* inventoryOfCase, int countCase) {
+    printf("Список продуктів:\n");
+    for (int i = 0; i < countCase; i++) {
+        printf("ID: %d, Назва: %s %s, Сумісний з %s, Ціна: %.2f грн\n", inventoryOfCase[i].id, inventoryOfCase[i].brand, inventoryOfCase[i].name, inventoryOfCase[i].compatibility, inventoryOfCase[i].price);
+    }
+}
+
+void listOfPowerBank(Product* inventoryOfPowerBank, int countPowerBank) {
+    printf("Список продуктів:\n");
+    for (int i = 0; i < countPowerBank; i++) {
+        printf("ID: %d, Назва: %s %s, Швидкість: %.0f W, Ціна: %.2f грн\n", inventoryOfPowerBank[i].id, inventoryOfPowerBank[i].brand, inventoryOfPowerBank[i].name, inventoryOfPowerBank[0].speed, inventoryOfPowerBank[i].price);
+    }
+}
+
 
 int main() {
     SetConsoleCP(1251);
@@ -56,43 +79,75 @@ int main() {
 
     int productId, quantity;
 
-    Product inventory[5];
+    Product inventoryOfPhones[5];
     int count = 5;
 
-    inventory[0].id = 1;
-    strcpy(inventory[0].brand, "Xiaomi");
-    strcpy(inventory[0].name, "Телефон 1");
-    inventory[0].memory = 128;
-    inventory[0].price = 100.0;
+    inventoryOfPhones[0].id = 1;
+    strcpy(inventoryOfPhones[0].brand, "Xiaomi");
+    strcpy(inventoryOfPhones[0].name, "Телефон 1");
+    inventoryOfPhones[0].memory = 128;
+    inventoryOfPhones[0].price = 100.0;
 
 
-    inventory[1].id = 2;
-    strcpy(inventory[0].brand, "Samsung");
-    strcpy(inventory[1].name, "Телефон 2");
-    inventory[0].memory = 128;
-    inventory[1].price = 200.0;
+    inventoryOfPhones[1].id = 2;
+    strcpy(inventoryOfPhones[0].brand, "Samsung");
+    strcpy(inventoryOfPhones[1].name, "Телефон 2");
+    inventoryOfPhones[0].memory = 128;
+    inventoryOfPhones[1].price = 200.0;
 
-    inventory[2].id = 3;
-    strcpy(inventory[0].brand, "Xiaomi");
-    strcpy(inventory[2].name, "Телефон 3");
-    inventory[0].memory = 64;
-    inventory[2].price = 150.0;
+    inventoryOfPhones[2].id = 3;
+    strcpy(inventoryOfPhones[0].brand, "Xiaomi");
+    strcpy(inventoryOfPhones[2].name, "Телефон 3");
+    inventoryOfPhones[0].memory = 64;
+    inventoryOfPhones[2].price = 150.0;
 
-    inventory[3].id = 4;
-    strcpy(inventory[0].brand, "Apple");
-    strcpy(inventory[3].name, "Телефон 4");
-    inventory[0].memory = 256;
-    inventory[3].price = 400.0;
+    inventoryOfPhones[3].id = 4;
+    strcpy(inventoryOfPhones[0].brand, "Apple");
+    strcpy(inventoryOfPhones[3].name, "Телефон 4");
+    inventoryOfPhones[0].memory = 256;
+    inventoryOfPhones[3].price = 400.0;
 
-    inventory[4].id = 5;
-    strcpy(inventory[0].brand, "Xiaomi");
-    strcpy(inventory[4].name, "Телефон 5");
-    inventory[0].memory = 128;
-    inventory[4].price = 250.0;
+    inventoryOfPhones[4].id = 5;
+    strcpy(inventoryOfPhones[0].brand, "Xiaomi"); // [0] !!!
+    strcpy(inventoryOfPhones[4].name, "Телефон 5");
+    inventoryOfPhones[0].memory = 128;
+    inventoryOfPhones[4].price = 250.0;
 
+
+    Product inventoryOfChargers[1];
+    int countChargers = 1;
+
+    inventoryOfChargers[0].id = 6;
+    strcpy(inventoryOfChargers[0].brand, "Xiaomi");
+    strcpy(inventoryOfChargers[0].name, "Швидкий шнур"); // РОЗЙОМ
+    inventoryOfChargers[0].leigth = 1;
+    inventoryOfPhones[0].price = 50.0;
+    
+    
+    Product inventoryOfCase[1];
+    int countCase = 1;
+
+    inventoryOfCase[0].id = 7;
+    strcpy(inventoryOfCase[0].brand, "Xiaomi");
+    strcpy(inventoryOfCase[0].name, "Super Case");
+    strcpy(inventoryOfCase[0].compatibility, "Телефон 2"); 
+    inventoryOfCase[0].price = 20.0;
+    
+    
+    Product inventoryOfPowerBank[1];
+    int countPowerBank = 1;
+
+    inventoryOfPowerBank[0].id = 8;
+    strcpy(inventoryOfPowerBank[0].brand, "Xiaomi");
+    strcpy(inventoryOfPowerBank[0].name, "Супер фаст чардж");
+    inventoryOfPowerBank[0].speed = 18;
+    inventoryOfPowerBank[0].price = 80.0;
+    
+    
+    
     CartItem cart[10];
     int cartSize = 0; 
-    int choice;
+    int choice, choiceOfCategory;
     float total = 0.0;
     char surname[20], name[20], middleName[20], sex[10], mail[100];
 
@@ -113,68 +168,90 @@ int main() {
     char menuChoice[15];
 
     do {
-    printf("Якщо ви бажаєте продовжити, впишіть \"так\", щоб покинути магазин напишіть \"покинути\": \n");
-    scanf("%s", &menuChoice);
+        printf("Якщо ви бажаєте продовжити використання магазину, впишіть \"продовжити\", щоб покинути напишіть \"покинути\": \n");
+        scanf("%s", menuChoice);
 
-    if (strcmp(menuChoice, "так") == 0) {
-    do {
-        printf("Меню:\n");
-        printf("1. Вивести список продуктів\n");
-        printf("2. Додати продукт до корзини\n");
-        printf("3. Переглянути вміст корзини\n");
-        printf("4. Вийти\n");
-        printf("Ваш вибір: ");
-        scanf("%d", &choice);
+        if (strcmp(menuChoice, "продовжити") == 0) {
+            do {
+                printf("Меню:\n");
+                printf("1. Перейти до покупок\n");
+                printf("2. Додати продукт до корзини\n");
+                printf("3. Переглянути вміст корзини\n");
+                printf("4. Вийти з меню\n");
+                printf("Ваш вибір: ");
+                scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                listProducts(inventory, count);
-                break;
-            case 2:
-                printf("Введіть ID продукту: ");
-                scanf("%d", &productId);
-                printf("Введіть кількість: ");
-                scanf("%d", &quantity);
+                switch (choice) {
+                    case 1:
+                        do {
+                            printf("Оберіть категорію товару, що вас цікавить\n");
+                            printf("1. Смартфони\n");
+                            printf("2. Зарядні пристрої\n");
+                            printf("3. Чохли\n");
+                            printf("4. Павербанки\n");
+                            printf("5. Повернутись назад\n");
+                            printf("Ваш вибір: ");
+                            scanf("%d", &choiceOfCategory);
 
-                int found = 0;
-                for (int i = 0; i < count; i++) {
-                    if (inventory[i].id == productId) {
-                        addToCart(&inventory[i], quantity, cart, &cartSize);
-                        found = 1;
+                            switch (choiceOfCategory) {
+                                case 1:
+                                    listOfPhones(inventoryOfPhones, count);
+                                    break;
+                                case 2:
+                                    listOfChargers(inventoryOfChargers, countChargers);
+                                    break;
+                                case 3:
+                                    listOfCase(inventoryOfCase, countCase);
+                                    break;
+                                case 4:
+                                    listOfPowerBank(inventoryOfPowerBank, countPowerBank);
+                                    break;
+                                case 5:
+                                    printf("Ви повернулись до головного меню\n");
+                                    break;
+                                default:
+                                    printf("Невірний вибір категорії. Спробуйте ще раз.\n");
+                                    break;
+                            }
+                        } while (choiceOfCategory != 5);
                         break;
-                    }
-                }
-                
-                for (int i = 0; i < count; i++) {
-                    if (inventory[i].id == productId) {
-                        addSumCart(&inventory[i], quantity, &total);
-                        found = 1;
-                        break;
-                    }
-                }
+                    case 2:
+                        printf("Введіть ID продукту: ");
+                        scanf("%d", &productId);
+                        printf("Введіть кількість: ");
+                        scanf("%d", &quantity);
 
-                if (!found) {
-                    printf("Продукт з ID %d не знайдено\n", productId);
+                        int found = 0;
+                        for (int i = 0; i < count; i++) {
+                            if (inventoryOfPhones[i].id == productId) {
+                                addToCart(&inventoryOfPhones[i], quantity, cart, &cartSize);
+                                addSumCart(&inventoryOfPhones[i], quantity, &total);
+                                found = 1;
+                                break;
+                            }
+                        }
+
+                        if (!found) {
+                            printf("Продукт з ID %d не знайдено\n", productId);
+                        }
+                        break;
+                    case 3:
+                        displayCart(cart, cartSize);
+                        printf("Загальна вартість корзини: %.2f\n", total);
+                        break;
+                    case 4:
+                        printf("Ви вийшли з меню\n");
+                        break;
+                    default:
+                        printf("Невірний вибір. Спробуйте ще раз.\n");
+                        break;
                 }
-                break;
-            case 3:
-                displayCart(cart, cartSize);
-                printf("Загальна вартість корзини: %.2f\n", total);
-                break;
-            case 4:
-                printf("Дякую за використання магазину!\n");
-                break;
-            default:
-                printf("Невірний вибір. Спробуйте ще раз.\n");
-                break;
-        }
-    } while (choice != 4);
-    }
-        else if (strcmp(menuChoice, "покинути") != 0) {
+            } while (choice != 4);
+        } else if (strcmp(menuChoice, "покинути") != 0) {
             printf("Невірна відповідь. Спробуйте ще раз.\n");
         }
     } while (strcmp(menuChoice, "покинути") != 0);
-
-
+    printf("Дякую за використання магазину!\n");
+    
     return 0;
 }
